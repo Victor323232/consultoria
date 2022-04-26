f26d66669c9b9527e7a84fb278bf3c92f000982a
Victor323232 <victorhugoabc59@gmail.com>
2022-04-25 23:05:05 -0300

    start commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:consultoria/primeirapagina.dart';
+import 'package:flutter/material.dart';
+
+  void main() {
+  runApp(
+      MaterialApp(
+        home: primeirapagina(),
+          )
+  );
+
+}
---
+++ lib/primeirapagina.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class primeirapagina extends StatefulWidget {
+  const primeirapagina({Key? key}) : super(key: key);
+
+  @override
+  State<primeirapagina> createState() => _primeirapaginaState();
+}
+
+class _primeirapaginaState extends State<primeirapagina> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
