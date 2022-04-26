19df00c0b2f094319097d5c277b52952da2c5ae6
Victor323232 <victorhugoabc59@gmail.com>
2022-04-26 20:52:01 -0300

    align container, add images, gesture detector

--- lib/primeirapagina.dart
+++ lib/primeirapagina.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class primeirapagina extends StatefulWidget {
@@ -8,8 +10,59 @@ class primeirapagina extends StatefulWidget {
 }
 
 class _primeirapaginaState extends State<primeirapagina> {
+
+  void _abrirEmpresa(){
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consultoria"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all( 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Padding(padding: EdgeInsets.only(top: 15),
+             child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, //separa com espaço
+                children: <Widget>[
+                  GestureDetector(
+                    onTap:_abrirEmpresa ,
+                    child: Image.asset("images/detalhe_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/detalhe_servico.png"),
+                  ),
+                ],
+              ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 15),
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap:_abrirEmpresa ,
+                      child: Image.asset("images/detalhe_cliente.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _abrirEmpresa,
+                      child: Image.asset("images/detalhe_contato.png"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+    );
   }
 }
